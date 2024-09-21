32dfffc6926ab7aea89ba8ffbe6534340783e8ec
YoucefBen47 <70854871+YoucefBen47@users.noreply.github.com>
2024-09-21 19:54:56 +0100

    Added first index screen

---
+++ components/ThemedText.tsx
@@ -0,0 +1,16 @@
+import { Colors } from "@/constants/Colors";
+import React from "react";
+import { Text, TextProps, useColorScheme } from "react-native";
+
+const ThemedText = ({ style, children, ...rest }: TextProps) => {
+  const colorScheme = useColorScheme();
+
+  const color = Colors[colorScheme ?? "light"].text;
+  return (
+    <Text style={[style, { color }]} {...rest}>
+      {children}
+    </Text>
+  );
+};
+
+export default ThemedText;
